8fdac4ea285d964f1e7b9d224f31399b4419179b
Hamza-Imanli <hamzaimanli2@gmail.com>
2023-11-30 16:53:48 +0400

    Ud

---
+++ NtireGarage/Presentation/Program.cs
@@ -0,0 +1,46 @@
+using Business.Services.Abstract;
+using Business.Services.Concrete;
+using Common.Entities;
+using DataAccess.Context;
+using DataAccess.Repositories.Abstract;
+using DataAccess.Repositories.Concrete;
+using DataAccess.UnitofWork;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(x=>x.UseSqlServer(builder.Configuration.GetConnectionString("Default"),x=>x.MigrationsAssembly(nameof(DataAccess))));
+
+builder.Services.AddIdentity<User,IdentityRole>(opt =>
+{
+    opt.Password.RequiredLength = 8;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IUnitofWork, UnitofWork>();
+builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+
+
+
+
+
+
+
+
+
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action}/{id?}"
+    );
+
+app.Run();
